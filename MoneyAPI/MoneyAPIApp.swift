fa062d9cf7fcd27bc08f38f90745b1556411907f
Joseph DeWeese <inclinedtactics@icloud.com>
2024-04-01 23:05:21 -0500

    Initial Commit

---
+++ MoneyAPI/MoneyAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyAPIApp.swift
+//  MoneyAPI
+//
+//  Created by Joseph DeWeese on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MoneyAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
